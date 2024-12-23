cc785a55652c4cf07c8b4b57127cd7f22703e851
이태훈 <rhymint@gmail.com>
2024-12-23 15:13:55 +0900

    fix: button style disabled인 경우 interaction이 없도록 변경

--- packages/ui/src/components/Button/Button.style.ts
+++ packages/ui/src/components/Button/Button.style.ts
@@ -21,41 +21,49 @@ export const buttonStyle = cva({
       primary: {
         bg: 'primary',
         color: 'white',
-        _hover: {
-          opacity: 0.8
-        },
-        _active: {
-          opacity: 0.9
+        _notDisabled: {
+          _hover: {
+            opacity: 0.8
+          },
+          _active: {
+            opacity: 0.9
+          }
         }
       },
       secondary: {
         bg: 'lightGray',
         color: 'light',
-        _hover: {
-          opacity: 0.8
-        },
-        _active: {
-          opacity: 0.9
+        _notDisabled: {
+          _hover: {
+            opacity: 0.8
+          },
+          _active: {
+            opacity: 0.9
+          }
         },
       },
       outline: {
         border: '1px solid',
         borderColor: 'light',
         color: 'light',
-        _hover: {
-          bg: 'rgba(230, 230, 230, 0.8)'
-        },
-        _active: {
-          bg: 'rgba(230, 230, 230, 0.9)'
+        _notDisabled: {
+          _hover: {
+            bg: 'rgba(230, 230, 230, 0.8)'
+          },
+          _active: {
+            bg: 'rgba(230, 230, 230, 0.9)'
+          }
         }
       },
       ghost: {
         color: 'light',
-        _hover: {
-          bg: 'rgba(230, 230, 230, 0.8)'
-        },
-        _active: {
-          bg: 'rgba(230, 230, 230, 0.9)'
+        _notDisabled: {
+          _hover: {
+            bg: 'rgba(230, 230, 230, 0.8)'
+          },
+          _active: {
+            bg: 'rgba(230, 230, 230, 0.9)'
+          }
         }
       }
     },
